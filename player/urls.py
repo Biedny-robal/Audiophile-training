a02be3c1bd6a26f5e08116d194accfa3fda40127
mladyzyn <michal.ladyzynski@gmail.com>
2026-06-02 00:02:29 +0200

    fixes

--- player/urls.py
+++ player/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.menu, name='menu'),
     path('eq-trainer/', views.index, name='index'),
+    path('loudness/', views.loudness, name='loudness'),
     path('audio/', views.serve_audio, name='serve_audio'),
+    path('raw-audio/', views.serve_raw_audio, name='serve_raw_audio'),
 ]
